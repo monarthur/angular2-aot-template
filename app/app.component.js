8212099f3aed6c1d4821fa448f21e1df97158d9b
monarthur <fsjogren@gmail.com>
2016-10-25 13:28:15 +0200

    fått aot och treeshaking att funka

---
+++ app/app.component.js
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+export var AppComponent = (function () {
+    function AppComponent() {
+        this.test = 'Apan lever ju';
+    }
+    AppComponent.decorators = [
+        { type: Component, args: [{
+                    moduleId: module.id,
+                    selector: 'my-app',
+                    templateUrl: 'app.component.html'
+                },] },
+    ];
+    /** @nocollapse */
+    AppComponent.ctorParameters = [];
+    return AppComponent;
+}());
+//# sourceMappingURL=app.component.js.map
