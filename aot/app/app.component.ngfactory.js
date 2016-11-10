345bde1af46b1106f55399b786a473d59c5231cc
monarthur <fsjogren@gmail.com>
2016-11-10 09:05:20 +0100

    test

--- aot/app/app.component.ngfactory.js
+++ aot/app/app.component.ngfactory.js
@@ -3,20 +3,21 @@
  * Do not edit.
  */
 /* tslint:disable */
+"use strict";
 var __extends = (this && this.__extends) || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
-import * as import0 from '../../app/app.component';
-import * as import1 from '@angular/core/src/linker/view';
-import * as import3 from '@angular/core/src/linker/element';
-import * as import4 from '@angular/core/src/linker/view_utils';
-import * as import6 from '@angular/core/src/linker/view_type';
-import * as import7 from '@angular/core/src/change_detection/change_detection';
-import * as import8 from '@angular/core/src/metadata/view';
-import * as import9 from '@angular/core/src/linker/component_factory';
-export var Wrapper_AppComponent = (function () {
+var import0 = require('../../app/app.component');
+var import1 = require('@angular/core/src/linker/view');
+var import3 = require('@angular/core/src/linker/element');
+var import4 = require('@angular/core/src/linker/view_utils');
+var import6 = require('@angular/core/src/linker/view_type');
+var import7 = require('@angular/core/src/change_detection/change_detection');
+var import8 = require('@angular/core/src/metadata/view');
+var import9 = require('@angular/core/src/linker/component_factory');
+var Wrapper_AppComponent = (function () {
     function Wrapper_AppComponent() {
         this.changed = false;
         this.context = new import0.AppComponent();
@@ -28,6 +29,7 @@ export var Wrapper_AppComponent = (function () {
     };
     return Wrapper_AppComponent;
 }());
+exports.Wrapper_AppComponent = Wrapper_AppComponent;
 var renderType_AppComponent_Host = null;
 var _View_AppComponent_Host0 = (function (_super) {
     __extends(_View_AppComponent_Host0, _super);
@@ -63,7 +65,7 @@ function viewFactory_AppComponent_Host0(viewUtils, parentInjector, declarationEl
     }
     return new _View_AppComponent_Host0(viewUtils, parentInjector, declarationEl);
 }
-export var AppComponentNgFactory = new import9.ComponentFactory('my-app', viewFactory_AppComponent_Host0, import0.AppComponent);
+exports.AppComponentNgFactory = new import9.ComponentFactory('my-app', viewFactory_AppComponent_Host0, import0.AppComponent);
 var styles_AppComponent = [];
 var renderType_AppComponent = null;
 var _View_AppComponent0 = (function (_super) {
@@ -99,10 +101,11 @@ var _View_AppComponent0 = (function (_super) {
     };
     return _View_AppComponent0;
 }(import1.AppView));
-export function viewFactory_AppComponent0(viewUtils, parentInjector, declarationEl) {
+function viewFactory_AppComponent0(viewUtils, parentInjector, declarationEl) {
     if ((renderType_AppComponent === null)) {
         (renderType_AppComponent = viewUtils.createRenderComponentType('', 0, import8.ViewEncapsulation.None, styles_AppComponent, {}));
     }
     return new _View_AppComponent0(viewUtils, parentInjector, declarationEl);
 }
+exports.viewFactory_AppComponent0 = viewFactory_AppComponent0;
 //# sourceMappingURL=app.component.ngfactory.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -5,5 +5,5 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-    test:String = 'Apan lever ju';
+    test:String = 'Apan lever ju faktiskt!';
  }
